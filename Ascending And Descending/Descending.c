0f9d69c321202c9a70ea468eecf52d3fa9cf555c
C0DEGamer <154326486+C0DEGamer@users.noreply.github.com>
2024-05-16 20:34:31 +0530

    Add files via upload

---
+++ Ascending And Descending/Descending.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+int a[1000],i,j,b,n;
+
+
+
+printf("How Many Numbers \n");
+scanf("%d",&n);
+printf("Enter The Numbers\n");
+
+//for loop for get numbers
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+
+
+}
+
+for(i=0;i<n;i++){
+    for(j=i+1;j<n;j++){
+
+       if(a[i]<a[j]){
+
+        b=a[i];
+        a[i]=a[j];
+        a[j]=b;
+
+
+       }
+
+
+    }
+}
+
+
+printf("Sorted Numbers In Descending Order\n ");
+
+for(i=0;i<n;i++){
+    printf("%d\t",a[i]);
+}
+
+return 0;
+}
